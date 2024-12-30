a85356c79b5c412b8b3efaf5b2a16f3d50637211
Georgi Grigorov <georgi.grigorov@limechain.tech>
2024-12-31 00:06:52 +0200

    add search functionality and tests for it

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,54 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
 
-    println!("Content: \n{}", content);
+    for line in search(&config.query, &content.as_str()) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, content: &'a &str) -> Vec<&'a str> {
+    // let mut result: Vec<&str> = Vec::new();
+    //
+    // for line in content.lines() {
+    //     if line.contains(query) {
+    //         result.push(line);
+    //     }
+    // }
+    //
+    // result
+    content.lines().filter(|line| line.contains(query)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        let result = search(query, &content);
+        assert_eq!(1, result.len());
+        assert_eq!(vec!["safe, fast, productive."], result);
+    }
+
+    #[test]
+    fn no_result() {
+        let query = "asd";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        let result = search(query, &content);
+        assert_eq!(0, result.len());
+    }
 }
 
 
